305376c9579cca3ee95eb26baa9a604bd29af76a
MiquelGalvez <mgalvez.dam@institutcampalans.net>
2024-03-15 18:38:56 +0100

    level1-shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,20 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed; // Velocidad de movimiento normal
     public float runSpeed; // Velocidad de movimiento al correr
     public float jumpForce; // Fuerza del salto
+    public GameObject bulletPrefab; // El prefab de la bala
+    public float bulletSpeed = 5; // Velocitat de la bala
+    private Camera cam;
 
     private Rigidbody2D rb;
     private bool isRunning;
     private bool isFacingRight = true;
+    private bool isGrounded; // Indica si el personaje está en el suelo
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>(); // Obtener la referencia al Animator
+        cam = Camera.main;
     }
 
     void Update()
@@ -26,16 +31,11 @@ public class PlayerController : MonoBehaviour
         float moveSpeedCurrent = isRunning ? runSpeed : moveSpeed;
         rb.velocity = new Vector2(moveInput * moveSpeedCurrent, rb.velocity.y);
 
-        // Saltar
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Saltar solo si está en el suelo
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             _animator.SetBool("IsJumping", true);
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, -jumpForce);
-            _animator.SetBool("IsJumping", false);
         }
 
         // Correr al presionar Shift
@@ -48,6 +48,30 @@ public class PlayerController : MonoBehaviour
         {
             isRunning = false;
             _animator.SetBool("IsRunning", false); // Establecer el parámetro en false
+        }
+
+        // Disparar al hacer clic con el ratón
+        if (Input.GetMouseButtonDown(0)) // Botón izquierdo del ratón
+        {
+            Shoot();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            _animator.SetBool("IsJumping", false); // Establecer el parámetro en false
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            _animator.SetBool("IsJumping", true); // Establecer el parámetro en true
         }
     }
 
@@ -71,6 +95,26 @@ public class PlayerController : MonoBehaviour
             Vector3 scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
+        }
+    }
+
+    void Shoot()
+    {
+        if (bulletPrefab != null)
+        {
+            // Crea l'objecte bala amb l'Instiante
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            // Agafa el rigid body del gameobject bullet
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+
+            // Calcula la direcció de la bala en funcio a si el player esta mirant a la dreta o l'esquerra
+            Vector2 shootDirection = isFacingRight ? Vector2.right : Vector2.left;
+
+            // Intsancia la velocitat de la bala i la multiplica en la direcció en la que esta mirant el jugador
+            bulletRb.velocity = shootDirection * bulletSpeed;
+
+            Destroy(bullet,2.5f);
         }
     }
 }
